ffed58764339ab6192b049230a9da29dce0cef0b
Andrew Lavin <aj.lavin@gmail.com>
2024-06-20 12:20:29 -0700

    Add stride=1 constants for innermost dimension.
    Add base class for all Index* base classes.

--- include/spio/index.h
+++ include/spio/index.h
@@ -3,76 +3,75 @@
 
 namespace spio
 {
-    /// A base class for a 2-dimensional index.
-    template <int _D1>
-    class Index2D
+    /// A base class for all Index* classes.
+    class IndexBase
     {
     public:
-        constexpr static int _D0_Stride = _D1;
-
-        constexpr Index2D(int offset = 0) : _offset(offset) {}
-
+        constexpr IndexBase(int offset = 0) : _offset(offset) {}
         constexpr operator int() const { return _offset; }
 
-        constexpr Index2D _d1(int d1) { return Index2D(_offset + d1); }
-        constexpr Index2D _d0(int d0) { return Index2D(_offset + d0 * _D0_Stride); }
-
-        constexpr int _d0() { return _offset / _D0_Stride; }
-        constexpr int _d1() { return _offset % _D1; }
-
-    private:
+    protected:
         const int _offset;
+    };
+
+    /// A base class for a 2-dimensional index.
+    template <int _D1>
+    class Index2D : public IndexBase
+    {
+    public:
+        constexpr static int _D1_Stride = 1;
+        constexpr static int _D0_Stride = _D1 * _D1_Stride;
+
+        using IndexBase::IndexBase;
+
+        constexpr Index2D _d1(int d1) const { return Index2D(_offset + d1 * _D1_Stride); }
+        constexpr Index2D _d0(int d0) const { return Index2D(_offset + d0 * _D0_Stride); }
+
+        constexpr int _d0() const { return _offset / _D0_Stride; }
+        constexpr int _d1() const { return (_offset / _D1_Stride) % _D1; }
     };
 
     /// A base class for a 3-dimensional index.
     template <int _D1, int _D2>
-    class Index3D
+    class Index3D : public IndexBase
     {
     public:
-        constexpr static int _D1_Stride = _D2;
-        constexpr static int _D0_Stride = _D1 * _D2;
+        constexpr static int _D2_Stride = 1;
+        constexpr static int _D1_Stride = _D2 * _D2_Stride;
+        constexpr static int _D0_Stride = _D1 * _D1_Stride;
 
-        constexpr Index3D(int offset = 0) : _offset(offset) {}
+        using IndexBase::IndexBase;
 
-        constexpr operator int() const { return _offset; }
+        constexpr Index3D _d2(int d2) const { return Index3D(_offset + d2 * _D2_Stride); }
+        constexpr Index3D _d1(int d1) const { return Index3D(_offset + d1 * _D1_Stride); }
+        constexpr Index3D _d0(int d0) const { return Index3D(_offset + d0 * _D0_Stride); }
 
-        constexpr Index3D _d2(int d2) { return Index3D(_offset + d2); }
-        constexpr Index3D _d1(int d1) { return Index3D(_offset + d1 * _D1_Stride); }
-        constexpr Index3D _d0(int d0) { return Index3D(_offset + d0 * _D0_Stride); }
-
-        constexpr int _d2() { return _offset % _D2; }
-        constexpr int _d1() { return (_offset / _D1_Stride) % _D1; }
-        constexpr int _d0() { return _offset / _D0_Stride; }
-
-    private:
-        const int _offset;
+        constexpr int _d2() const { return (_offset / _D2_Stride) % _D2; }
+        constexpr int _d1() const { return (_offset / _D1_Stride) % _D1; }
+        constexpr int _d0() const { return _offset / _D0_Stride; }
     };
 
     /// A base class for a 4-dimensional index.
     template <int _D1, int _D2, int _D3>
-    class Index4D
+    class Index4D : public IndexBase
     {
     public:
-        constexpr static int _D2_Stride = _D3;
-        constexpr static int _D1_Stride = _D2 * _D3;
-        constexpr static int _D0_Stride = _D1 * _D2 * _D3;
+        constexpr static int _D3_Stride = 1;
+        constexpr static int _D2_Stride = _D3 * _D3_Stride;
+        constexpr static int _D1_Stride = _D2 * _D2_Stride;
+        constexpr static int _D0_Stride = _D1 * _D1_Stride;
 
-        constexpr Index4D(int offset = 0) : _offset(offset) {}
+        using IndexBase::IndexBase;
 
-        constexpr operator int() const { return _offset; }
+        constexpr Index4D _d3(int d3) const { return Index4D(_offset + d3 * _D3_Stride); }
+        constexpr Index4D _d2(int d2) const { return Index4D(_offset + d2 * _D2_Stride); }
+        constexpr Index4D _d1(int d1) const { return Index4D(_offset + d1 * _D1_Stride); }
+        constexpr Index4D _d0(int d0) const { return Index4D(_offset + d0 * _D0_Stride); }
 
-        constexpr Index4D _d3(int d3) { return Index4D(_offset + d3); }
-        constexpr Index4D _d2(int d2) { return Index4D(_offset + d2 * _D2_Stride); }
-        constexpr Index4D _d1(int d1) { return Index4D(_offset + d1 * _D1_Stride); }
-        constexpr Index4D _d0(int d0) { return Index4D(_offset + d0 * _D0_Stride); }
-
-        constexpr int _d3() { return _offset % _D3; }
-        constexpr int _d2() { return (_offset / _D2_Stride) % _D2; }
-        constexpr int _d1() { return (_offset / _D1_Stride) % _D1; }
-        constexpr int _d0() { return _offset / _D0_Stride; }
-
-    private:
-        const int _offset;
+        constexpr int _d3() const { return (_offset / _D3_Stride) % _D3; }
+        constexpr int _d2() const { return (_offset / _D2_Stride) % _D2; }
+        constexpr int _d1() const { return (_offset / _D1_Stride) % _D1; }
+        constexpr int _d0() const { return _offset / _D0_Stride; }
     };
 }
 #endif
