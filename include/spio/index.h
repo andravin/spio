7d83c433a1048e509d1c34c030111be275993b78
Andrew Lavin <aj.lavin@gmail.com>
2024-06-20 11:21:43 -0700

    Ensure all members are prefixed with underscores in the Index* base classes.

--- include/spio/index.h
+++ include/spio/index.h
@@ -8,18 +8,17 @@ namespace spio
     class Index2D
     {
     public:
-        constexpr static int D1 = _D1;
-        constexpr static int D0_Stride = D1;
+        constexpr static int _D0_Stride = _D1;
 
         constexpr Index2D(int offset = 0) : _offset(offset) {}
 
         constexpr operator int() const { return _offset; }
 
         constexpr Index2D _d1(int d1) { return Index2D(_offset + d1); }
-        constexpr Index2D _d0(int d0) { return Index2D(_offset + d0 * D0_Stride); }
+        constexpr Index2D _d0(int d0) { return Index2D(_offset + d0 * _D0_Stride); }
 
-        constexpr int _d0() { return _offset / D0_Stride; }
-        constexpr int _d1() { return _offset % D1; }
+        constexpr int _d0() { return _offset / _D0_Stride; }
+        constexpr int _d1() { return _offset % _D1; }
 
     private:
         const int _offset;
@@ -30,23 +29,20 @@ namespace spio
     class Index3D
     {
     public:
-        constexpr static int D1 = _D1;
-        constexpr static int D2 = _D2;
-
-        constexpr static int D1_Stride = D2;
-        constexpr static int D0_Stride = D1 * D2;
+        constexpr static int _D1_Stride = _D2;
+        constexpr static int _D0_Stride = _D1 * _D2;
 
         constexpr Index3D(int offset = 0) : _offset(offset) {}
 
         constexpr operator int() const { return _offset; }
 
         constexpr Index3D _d2(int d2) { return Index3D(_offset + d2); }
-        constexpr Index3D _d1(int d1) { return Index3D(_offset + d1 * D1_Stride); }
-        constexpr Index3D _d0(int d0) { return Index3D(_offset + d0 * D0_Stride); }
+        constexpr Index3D _d1(int d1) { return Index3D(_offset + d1 * _D1_Stride); }
+        constexpr Index3D _d0(int d0) { return Index3D(_offset + d0 * _D0_Stride); }
 
-        constexpr int _d2() { return _offset % D2; }
-        constexpr int _d1() { return (_offset / D1_Stride) % D1; }
-        constexpr int _d0() { return _offset / D0_Stride; }
+        constexpr int _d2() { return _offset % _D2; }
+        constexpr int _d1() { return (_offset / _D1_Stride) % _D1; }
+        constexpr int _d0() { return _offset / _D0_Stride; }
 
     private:
         const int _offset;
