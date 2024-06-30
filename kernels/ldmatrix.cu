f68160a9873f17646d4113f94f263d40ddf920ce
Andrew Lavin <aj.lavin@gmail.com>
2024-06-30 16:05:58 -0700

    Add support for ldmatrix.trans instructions.

--- kernels/ldmatrix.cu
+++ kernels/ldmatrix.cu
@@ -9,7 +9,7 @@ extern "C"
     /// @param out The output matrix fragment in the order as stored in registers.
     /// @param in The input 8x8 matrix with float16 elements.
     /// @return
-    __global__ void ldmatrix_test(
+    __global__ void ldmatrix_x1_test(
         unsigned *__restrict__ out,
         const unsigned *__restrict__ in)
     {
@@ -33,7 +33,8 @@ extern "C"
 
         int lane = threadIdx.x % 32;
 
-        for(int idx = lane; idx < 32 * 2; idx += 32) {
+        for (int idx = lane; idx < 32 * 2; idx += 32)
+        {
             smem[idx] = in[idx];
         }
         __syncthreads();
@@ -46,7 +47,6 @@ extern "C"
         out[lane + 32] = a.y;
     }
 
-
     __global__ void ldmatrix_x4_test(
         unsigned *__restrict__ out,
         const unsigned *__restrict__ in)
@@ -55,7 +55,8 @@ extern "C"
 
         int lane = threadIdx.x % 32;
 
-        for(int idx = lane; idx < 32 * 4; idx += 32) {
+        for (int idx = lane; idx < 32 * 4; idx += 32)
+        {
             smem[idx] = in[idx];
         }
         __syncthreads();
@@ -63,6 +64,68 @@ extern "C"
         int fragment_idx = (lane / 8) % 4;
         int row_idx = lane % 8;
         uint4 a = ldmatrix_x4(smem + row_idx * 4 + fragment_idx * 32);
+
+        out[lane] = a.x;
+        out[lane + 32] = a.y;
+        out[lane + 64] = a.z;
+        out[lane + 96] = a.w;
+    }
+
+    __global__ void ldmatrix_x1_trans_test(
+        unsigned *__restrict__ out,
+        const unsigned *__restrict__ in)
+    {
+        __shared__ unsigned smem[32];
+
+        int lane = threadIdx.x % 32;
+
+        smem[threadIdx.x] = in[lane];
+        __syncthreads();
+
+        unsigned a = ldmatrix_x1_trans(smem + (lane % 8) * 4);
+
+        out[lane] = a;
+    }
+
+    __global__ void ldmatrix_x2_trans_test(
+        unsigned *__restrict__ out,
+        const unsigned *__restrict__ in)
+    {
+        __shared__ unsigned smem[32 * 2];
+
+        int lane = threadIdx.x % 32;
+
+        for (int idx = lane; idx < 32 * 2; idx += 32)
+        {
+            smem[idx] = in[idx];
+        }
+        __syncthreads();
+
+        int fragment_idx = (lane / 8) % 2;
+        int row_idx = lane % 8;
+        uint2 a = ldmatrix_x2_trans(smem + row_idx * 4 + fragment_idx * 32);
+
+        out[lane] = a.x;
+        out[lane + 32] = a.y;
+    }
+
+    __global__ void ldmatrix_x4_trans_test(
+        unsigned *__restrict__ out,
+        const unsigned *__restrict__ in)
+    {
+        __shared__ unsigned smem[32 * 4];
+
+        int lane = threadIdx.x % 32;
+
+        for (int idx = lane; idx < 32 * 4; idx += 32)
+        {
+            smem[idx] = in[idx];
+        }
+        __syncthreads();
+
+        int fragment_idx = (lane / 8) % 4;
+        int row_idx = lane % 8;
+        uint4 a = ldmatrix_x4_trans(smem + row_idx * 4 + fragment_idx * 32);
 
         out[lane] = a.x;
         out[lane + 32] = a.y;
--- tests/test_ldmatrix.py
+++ tests/test_ldmatrix.py
@@ -19,9 +19,11 @@ def _col(lane: int) -> int:
     return (lane % 4) * 2
 
 
-def test_ldmatrix_kernel():
+def test_ldmatrix_x1_kernel():
     """Compile and run an ldmatrix test kernel."""
-    module, ldmatrix_kernel = compile_test_kernel(kernel_name="ldmatrix")
+    module, ldmatrix_kernel = compile_test_kernel(
+        kernel_name="ldmatrix_x1", source_file_name="ldmatrix"
+    )
 
     a = cp.arange(8 * 8, dtype=cp.float16).reshape(8, 8)
     b = cp.zeros((64,), dtype=cp.float16)
@@ -74,3 +76,62 @@ def test_ldmatrix_x4_kernel():
             idx = lane * 2 + fragment * 64
             assert b[idx + 0] == a[fragment * 8 + row, col]
             assert b[idx + 1] == a[fragment * 8 + row, col + 1]
+
+
+def test_ldmatrix_x1_trans_kernel():
+    """Compile and run an ldmatrix_x1_trans test kernel."""
+    module, ldmatrix_x1_trans_kernel = compile_test_kernel(
+        kernel_name="ldmatrix_x1_trans", source_file_name="ldmatrix"
+    )
+
+    a = cp.arange(8 * 8, dtype=cp.float16).reshape(8, 8)
+    b = cp.zeros((64,), dtype=cp.float16)
+
+    ldmatrix_x1_trans_kernel((1,), (32,), (b, a))
+
+    for lane in range(32):
+        row = _row(lane)
+        col = _col(lane)
+        idx = lane * 2
+        assert b[idx + 0] == a[col, row]
+        assert b[idx + 1] == a[col + 1, row]
+
+
+def test_ldmatrix_x2_trans_kernel():
+    """Compile and run an ldmatrix_x2 test kernel."""
+    module, ldmatrix_x2_trans_kernel = compile_test_kernel(
+        kernel_name="ldmatrix_x2_trans", source_file_name="ldmatrix"
+    )
+
+    a = cp.arange(16 * 8, dtype=cp.float16).reshape(16, 8)
+    b = cp.zeros((16 * 8,), dtype=cp.float16)
+
+    ldmatrix_x2_trans_kernel((1,), (32,), (b, a))
+
+    for lane in range(32):
+        row = _row(lane)
+        col = _col(lane)
+        for fragment in range(2):
+            idx = lane * 2 + fragment * 64
+            assert b[idx + 0] == a[fragment * 8 + col, row]
+            assert b[idx + 1] == a[fragment * 8 + col + 1, row]
+
+
+def test_ldmatrix_x4_trans_kernel():
+    """Compile and run an ldmatrix_x4_trans test kernel."""
+    module, ldmatrix_x4_trans_kernel = compile_test_kernel(
+        kernel_name="ldmatrix_x4_trans", source_file_name="ldmatrix"
+    )
+
+    a = cp.arange(64 * 8, dtype=cp.float16).reshape(64, 8)
+    b = cp.zeros((64 * 8,), dtype=cp.float16)
+
+    ldmatrix_x4_trans_kernel((1,), (32,), (b, a))
+
+    for lane in range(32):
+        row = _row(lane)
+        col = _col(lane)
+        for fragment in range(4):
+            idx = lane * 2 + fragment * 64
+            assert b[idx + 0] == a[fragment * 8 + col, row]
+            assert b[idx + 1] == a[fragment * 8 + col + 1, row]
