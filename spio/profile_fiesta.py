2efc887d638f1679a47c25b8c185d0ad9a90d255
Andrew Lavin <aj.lavin@gmail.com>
2024-08-21 13:52:07 -0700

    Add timm model profiler.
    Rename the fiesta profiler.
    
    Enable cudnn.benchmark when profiling.

--- spio/profile_fiesta.py
+++ spio/profile_fiesta.py
@@ -1,21 +1,17 @@
 import argparse
+import sys
 
 import torch
 from torch import nn
 import cupy
 
-BATCH_SIZE = 128
-HEIGHT = 256
-WIDTH = 256
 GROUP_WIDTH = 4
 CHANNELS_LAST = True
 WARMUP_ITERS = 10
 BENCHMARK_ITERS = 1
 TOTAL_ITERS = WARMUP_ITERS + BENCHMARK_ITERS
-DEPTH = 1
 CHANNELS = 64
 CUDA_PROFILE = False
-USE_BIAS = False
 
 
 parser = argparse.ArgumentParser()
@@ -23,7 +19,14 @@ parser.add_argument("--group-width", type=int, default=GROUP_WIDTH)
 parser.add_argument("--channels", type=int, default=CHANNELS)
 parser.add_argument("--compile", action="store_true")
 parser.add_argument("--channels-first", action="store_true")
+parser.add_argument("--use-bias", action="store_true")
+parser.add_argument("--resolution", type=int, default=256)
+parser.add_argument("--depth", type=int, default=1)
+parser.add_argument("--summary", action="store_true")
+parser.add_argument("--batch-size", type=int, default=128)
 args = parser.parse_args()
+
+torch.backends.cudnn.benchmark = True
 
 memory_format = torch.contiguous_format if args.channels_first else torch.channels_last
 memory_format_name = (
@@ -32,39 +35,71 @@ memory_format_name = (
 
 
 class GroupedConvFiesta(nn.Module):
-    def __init__(self, num_channels=64, group_width=8, depth=16, bias=True):
+    def __init__(self, num_channels=64, group_width=8, depth=16, bias=False, expansion=4):
         super().__init__()
         groups = num_channels // group_width
-        self.stem = nn.Conv2d(3, num_channels, kernel_size=4, stride=4, padding=0)
+        mid_channels = num_channels * expansion
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, num_channels, kernel_size=4, stride=4, padding=0, bias=bias),
+            nn.BatchNorm2d(num_channels),
+            nn.ReLU(),
+        )
         convs = [
             nn.Sequential(
                 nn.Conv2d(
-                    num_channels, num_channels, kernel_size=3, groups=groups, padding=1
+                    num_channels, num_channels, kernel_size=3, groups=groups, padding=1, bias=bias
                 ),
                 nn.BatchNorm2d(num_channels),
                 nn.ReLU(),
+                nn.Conv2d(num_channels, mid_channels, kernel_size=1, bias=bias),
+                nn.BatchNorm2d(mid_channels),
+                nn.ReLU(),
+                nn.Conv2d(mid_channels, num_channels, kernel_size=1, bias=bias),
+                nn.BatchNorm2d(num_channels),
             )
             for d in range(depth)
         ]
         self.convs = nn.Sequential(*convs)
+        self.pool = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x):
         x = self.stem(x)
         x = self.convs(x)
+        x = self.pool(x)
         return x
 
 
 model = GroupedConvFiesta(
-    group_width=args.group_width, depth=DEPTH, num_channels=args.channels, bias=USE_BIAS
+    group_width=args.group_width, depth=args.depth, num_channels=args.channels, bias=args.use_bias
 ).cuda()
 
 model = model.to(memory_format=memory_format)
+input_size = (3, args.resolution, args.resolution)
 inputs = [
-    torch.randn((BATCH_SIZE, 3, HEIGHT, WIDTH), device="cuda").to(
+    torch.randn((args.batch_size, *input_size), device="cuda", dtype=torch.float16).to(
         memory_format=memory_format
     )
     for _ in range(TOTAL_ITERS)
 ]
+
+if args.summary:
+    import torchinfo
+    torchinfo.summary(
+        model,
+        input_size=(args.batch_size, *input_size),
+        depth=99,
+        col_names=[
+            "input_size",
+            "output_size",
+            "num_params",
+            "params_percent",
+            "kernel_size",
+            "mult_adds",
+            "trainable",
+        ],
+    )
+    sys.exit(0)
+
 
 if args.compile:
     model = torch.compile(model)
@@ -94,5 +129,5 @@ with torch.autocast(device_type="cuda", dtype=torch.float16):
                 prof.step()
         compiled = "compiled" if args.compile else "uncompiled"
         prof.export_chrome_trace(
-            f"trace_gw{args.group_width}_{memory_format_name}_{compiled}.json"
+            f"fiesta2_gw{args.group_width}_d{args.depth}_c{args.channels}_hw{args.resolution}-{memory_format_name}_{compiled}.json"
         )
---
+++ spio/profile_timm.py
@@ -0,0 +1,79 @@
+import argparse
+import sys
+
+import torch
+import timm
+
+
+def fwd_bwd(inp):
+    out = model(inp)
+    out.sum().backward()
+
+
+def trace_file_name(args):
+    memory_format_name = "channels_last" if args.channels_last else "channels_first"
+    compiled = "compiled" if args.compile else "not-compiled"
+    return (
+        f"trace_{args.model}_{memory_format_name}_{compiled}_bs{args.batch_size}.json"
+    )
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, default="convnext_base")
+parser.add_argument("--pretrained", action="store_true")
+parser.add_argument("--channels-last", action="store_true")
+parser.add_argument("--compile", action="store_true")
+parser.add_argument("--batch-size", type=int, default=128)
+parser.add_argument("--warmup-iters", type=int, default=10)
+parser.add_argument("--benchmark-iters", type=int, default=1)
+parser.add_argument("--summary", action="store_true")
+args = parser.parse_args()
+
+torch.backends.cudnn.benchmark = True
+
+memory_format = torch.channels_last if args.channels_last else torch.contiguous_format
+total_iters = args.warmup_iters + args.benchmark_iters
+
+model = timm.create_model(args.model, pretrained=args.pretrained).cuda()
+
+training_input_size = model.default_cfg["input_size"]
+
+inputs = [
+    torch.randn((args.batch_size, *training_input_size), device="cuda").to(
+        memory_format=memory_format
+    )
+    for _ in range(total_iters)
+]
+
+model = model.to(memory_format=memory_format)
+
+if args.summary:
+    import torchinfo
+    torchinfo.summary(
+        model,
+        input_size=(args.batch_size, *training_input_size),
+        depth=99,
+        col_names=[
+            "input_size",
+            "output_size",
+            "num_params",
+            "params_percent",
+            "kernel_size",
+            "mult_adds",
+            "trainable",
+        ],
+    )
+    sys.exit(0)
+
+if args.compile:
+    model = torch.compile(model)
+
+with torch.autocast(device_type="cuda", dtype=torch.float16):
+    for i in range(args.warmup_iters):
+        fwd_bwd(inputs[i])
+
+    with torch.profiler.profile() as prof:
+        for i in range(args.warmup_iters, total_iters):
+            fwd_bwd(inputs[i])
+            prof.step()
+    prof.export_chrome_trace(trace_file_name(args))
