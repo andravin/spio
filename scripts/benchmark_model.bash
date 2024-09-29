392ce26aa415458d4121426317d595d1c3a6f03f
Andrew Lavin <aj.lavin@gmail.com>
2024-09-29 12:52:07 -0700

    Add COMPILEMODE option to the benchmark_mode.bash script.

--- scripts/benchmark_model.bash
+++ scripts/benchmark_model.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 MODEL=$1
 BATCH_SIZE=$2
-BENCH={3:-train}
-SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio --torchcompile
+BENCH=${3:-train}
+COMPILEMODE=${4:-default}
+
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio --torchcompile --torchcompile-mode=$COMPILEMODE
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE --torchcompile
-SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --torchcompile
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --torchcompile --torchcompile-mode=$COMPILEMODE
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8
