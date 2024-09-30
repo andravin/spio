2cfa9c9c493f60b34a3320ec59fb01c63a3f3380
Andrew Lavin <aj.lavin@gmail.com>
2024-09-29 19:47:04 -0700

    Modify the benchmark_model.bash script to always try torchcompile with reduce-overhead versus default mode versus PyTorch greedy.

--- scripts/benchmark_model.bash
+++ scripts/benchmark_model.bash
@@ -2,11 +2,15 @@
 MODEL=$1
 BATCH_SIZE=$2
 BENCH=${3:-train}
-COMPILEMODE=${4:-default}
 
-SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio --torchcompile --torchcompile-mode=$COMPILEMODE
-SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE --torchcompile
-SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --torchcompile --torchcompile-mode=$COMPILEMODE
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio --torchcompile --torchcompile-mode=reduce-overhead
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE --torchcompile  --torchcompile-mode=reduce-overhead
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --torchcompile --torchcompile-mode=reduce-overhead
+
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio --torchcompile --torchcompile-mode=default
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE --torchcompile  --torchcompile-mode=default
+SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --torchcompile --torchcompile-mode=default
+
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8 --spio
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE
 SPIO_LOGGER=1 python3 benchmark.py --model $MODEL --amp --channels-last --bench=$BENCH   --batch-size=$BATCH_SIZE  --model-kwargs group_size=8
