65468f9eea1dd9053caa9223079b1367600b209b
Andrew Lavin <aj.lavin@gmail.com>
2024-10-07 18:28:35 -0700

    Add scripts for kernel benchmarks with models for training performance models.

---
+++ scripts/profile_kernels_in_models/convnext.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MAX_BATCH_SIZE=${1:-256}
+
+
+source profile_kernels_with_model.bash
+
+batch_size_convnext_tiny=$((MAX_BATCH_SIZE))
+batch_size_convnext_small=$((batch_size_convnext_tiny / 2))
+batch_size_convnext_medium=$((batch_size_convnext_small / 2))
+
+extra="--timm-model-kwargs group_size=8 kernel_sizes=5"
+
+# profile_kernels_with_model "convnext_tiny"  $batch_size_convnext_tiny $extra
+# profile_kernels_with_model "convnext_small"  $batch_size_convnext_small $extra
+profile_kernels_with_model "convnext_base"  $batch_size_convnext_medium $extra
---
+++ scripts/profile_kernels_in_models/profile_kernels_with_model.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPTS_DIR=..
+
+function profile_kernels_with_model() {
+    model=$1
+    shift
+    max_batch_size=$1
+    shift
+    extra=$@
+    for batch_size in 32 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512
+    do
+        if [ $batch_size -gt $max_batch_size ]
+        then
+            break
+        fi
+        echo "Benchmarking ${model} with batch size ${batch_size} .."
+        time python $SCRIPTS_DIR/profile_fiesta.py --batch-size=${batch_size} --timm-model=${model} --spio --benchmark-configs ${extra}
+        echo ".. done."
+        echo
+    done
+}
