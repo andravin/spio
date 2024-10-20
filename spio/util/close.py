91ff7dfe37a1b85432e06101cefa990f0ebf0b22
Andrew Lavin <aj.lavin@gmail.com>
2024-10-19 23:01:50 -0700

    Add atol fudge factor.
    Set atol_fudge to 3.5.

--- spio/util/close.py
+++ spio/util/close.py
@@ -1,13 +1,19 @@
 import torch
 
 
-def assert_all_close_with_acc_depth(actual, expected, msg=None, acc_depth=None, abs_mean=1.6, fudge=2.5):
+def assert_all_close_with_acc_depth(
+    actual,
+    expected,
+    msg=None,
+    acc_depth=None,
+    abs_mean=1.6,
+    atol_fudge=3.5,
+    rtol_fudge=2.0,
+):
     float16_precision = 5e-4
-    float32_precision = 1.19e-7 
-    atol = acc_depth * abs_mean * abs_mean * float32_precision
-    # We see rounding errors larger than float16 precision in some cases.
-    # Let's fudge it a bit.
-    rtol = float16_precision * fudge
+    float32_precision = 1.19e-7
+    atol = acc_depth * abs_mean * abs_mean * float32_precision * atol_fudge
+    rtol = float16_precision * rtol_fudge
     assert_all_close(actual, expected, atol=atol, rtol=rtol, msg=msg)
 
 
@@ -25,7 +31,10 @@ def assert_all_close(actual, expected, atol=0, rtol=0, msg=None):
         m = f"Tensors not close: atol={atol}, rtol={rtol}\n"
         max_errors = 30
         for a, e, ad, adt in zip(
-            bad_actual[:max_errors], bad_expected[:max_errors], bad_absdiff[:max_errors], bad_absdiff_tol[:max_errors]
+            bad_actual[:max_errors],
+            bad_expected[:max_errors],
+            bad_absdiff[:max_errors],
+            bad_absdiff_tol[:max_errors],
         ):
             ratio = ad / abs(e)
             m += f"actual ={a:>10.6f} expected ={e:>10.6f} |diff| = {ad:>10.6f} > abs_diff_tol = {adt:>10.6f}\n"
