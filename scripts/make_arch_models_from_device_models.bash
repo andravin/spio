3d4078a4636d049b6104bb5fb076556c7adb2df4
Andrew Lavin <aj.lavin@gmail.com>
2024-10-23 15:42:58 -0700

    Change file rename to file copy in make_arch_models_from_device_models.bash.

--- scripts/make_arch_models_from_device_models.bash
+++ scripts/make_arch_models_from_device_models.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-rename 's/devicemodel__.*__sm/archmodel__sm/' *.ubj
+
+for file in *.ubj; do
+  new_name=$(echo "$file" | sed 's/devicemodel__.*__sm/archmodel__sm/')
+  cp "$file" "$new_name"
+done
