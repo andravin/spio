ab903e9db733d2fc6c583d40fac1a1ed9563374b
Andrew Lavin <aj.lavin@gmail.com>
2024-10-25 19:11:14 -0700

    Add docstring.
    Reformat.

--- spio/compiler/__init__.py
+++ spio/compiler/__init__.py
@@ -1,3 +1,5 @@
+"""Functions for compiling CUDA kernels."""
+
 from .compile import compile_cuda
 from .compile_nvcc import nvcc_full_path, compile_with_nvcc
 from .compile_kernel import compile_kernel, load_kernel, compile_and_load_kernel
--- spio/src_tests/__init__.py
+++ spio/src_tests/__init__.py
@@ -1,4 +1,5 @@
 """Helpers for testing spio kernels, functions, and layers."""
+
 from .preprocess_data_file import preprocess_data_file, preprocess_data_string
 from .run_test import (
     run_function_test,
@@ -6,5 +7,5 @@ from .run_test import (
     run_kernel_test,
     run_grad_kernel_test,
     run_layer_test,
-    run_opcheck_test
+    run_opcheck_test,
 )
