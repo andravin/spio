68ec83dd9966ab239d412bf927af103381af22d2
Andrew Lavin <aj.lavin@gmail.com>
2024-10-26 21:25:48 -0700

    Fix Stats test.
    The parameter names had not been changed to lowercase for consistency
    with the new naming convention.

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -5,7 +5,7 @@ from spio.kernels.conv2d_gw8_kernel import conv2d_gw8_kernel_factory
 
 def test_conv2d_stats_fprop():
     """Test the statistics for the forward pass of the conv2d kernel."""
-    params = conv2d_gw8_kernel_factory.params_cls(N=16, H=32, W=64, C=128, R=3, S=3)
+    params = conv2d_gw8_kernel_factory.params_cls(n=16, h=32, w=64, c=128, r=3, s=3)
     stats = conv2d_gw8_kernel_factory.stats_cls(params, output_names="output")
     read = (16 * 32 * 64 * 128 + 128 * 8 * 3 * 3) * 2
     written = 16 * 32 * 64 * 128 * 2
@@ -18,7 +18,7 @@ def test_conv2d_stats_fprop():
 
 def test_conv2d_stats_input_grad():
     """Test the statistics for the input gradient of the conv2d kernel."""
-    params = conv2d_gw8_kernel_factory.params_cls(N=16, H=32, W=64, C=128, R=3, S=3)
+    params = conv2d_gw8_kernel_factory.params_cls(n=16, h=32, w=64, c=128, r=3, s=3)
     stats = conv2d_gw8_kernel_factory.stats_cls(params, output_names="grad_input")
     read = (16 * 32 * 64 * 128 + 128 * 8 * 3 * 3) * 2
     written = 16 * 32 * 64 * 128 * 2
@@ -31,7 +31,7 @@ def test_conv2d_stats_input_grad():
 
 def test_conv2d_stats_weight_grad():
     """Test the statistics for the weight gradient of the conv2d kernel."""
-    params = conv2d_gw8_kernel_factory.params_cls(N=16, H=32, W=64, C=128, R=3, S=3)
+    params = conv2d_gw8_kernel_factory.params_cls(n=16, h=32, w=64, c=128, r=3, s=3)
     stats = conv2d_gw8_kernel_factory.stats_cls(params, output_names="grad_weight")
     read = (16 * 32 * 64 * 128 + 16 * 32 * 64 * 128) * 2
     written = 128 * 8 * 3 * 3 * 2
@@ -44,6 +44,6 @@ def test_conv2d_stats_weight_grad():
 
 def test_conv2d_stats_bias_grad():
     """Test the statistics for the bias gradient of the conv2d kernel."""
-    params = conv2d_gw8_kernel_factory.params_cls(N=16, H=32, W=64, C=128, R=3, S=3)
+    params = conv2d_gw8_kernel_factory.params_cls(n=16, h=32, w=64, c=128, r=3, s=3)
     stats = conv2d_gw8_kernel_factory.stats_cls(params, output_names="grad_bias")
     assert stats.accumulation_depths == [16 * 32 * 64]
