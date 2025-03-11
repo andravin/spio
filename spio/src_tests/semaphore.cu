00e738394be407ad11d3ae00945348ce81dcae8b
Andrew Lavin <aj.lavin@gmail.com>
2025-03-11 11:19:38 -0700

    Fix warp_semaphore() test kernel to only record timestamps per-warp instead of per-thread.

--- spio/src_tests/semaphore.cu
+++ spio/src_tests/semaphore.cu
@@ -14,11 +14,13 @@ extern "C"
         __shared__ unsigned next_reservation;
         __shared__ unsigned next_execution;
 
-        spio::WarpSemaphore<warps> sem(slots, &next_reservation, &next_execution, max_count, threadIdx.x);
+        spio::WarpSemaphore<warps> sem(
+            slots, &next_reservation, &next_execution, max_count, threadIdx.x);
 
         __syncthreads();
 
         auto warp_idx = threadIdx.x / 32;
+        auto lane_idx = threadIdx.x % 32;
 
         event_types += warp_idx * events_per_warp;
         event_times += warp_idx * events_per_warp;
@@ -27,14 +29,18 @@ extern "C"
         {
             sem.acquire();
 
-            event_types[i * 2 + 0] = 1;
-            event_times[i * 2 + 0] = clock64();
+            if (lane_idx == 0)
+            {
+                event_types[i * 2 + 0] = 1;
+                event_times[i * 2 + 0] = clock64();
 
-            auto sleepy_time = ((i * 2 + threadIdx.x) % 100) + 50;
-            __nanosleep(sleepy_time);
+                auto sleepy_time = ((i * 2 + threadIdx.x) % 100) + 50;
+                __nanosleep(sleepy_time);
 
-            event_types[i * 2 + 1] = 0;
-            event_times[i * 2 + 1] = clock64();
+                event_types[i * 2 + 1] = 0;
+                event_times[i * 2 + 1] = clock64();
+            }
+            __syncwarp();
 
             sem.release();
         }
