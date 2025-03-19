7acb9524e302a8100bb0207dd8e721d3d12a71de
Andrew Lavin <aj.lavin@gmail.com>
2025-03-19 15:55:52 -0700

    Add template parameters for the dimension types to the mma_trans() functions.

--- spio/include/spio/fragment_mma.cuh
+++ spio/include/spio/fragment_mma.cuh
@@ -12,7 +12,8 @@ namespace spio
     /// @param a Matrix argument A is a 16 x 8 fragment in float16.
     /// @param b Matrix argument B is an 8 x 8 fragment in float16.
     /// @param c Accumulate matrix C is a 16 x 8 fragment in float32.
-    __device__ void mma_trans(MMA_M16_N8_F32_C &d, const MMA_M16_K8_F16_A &a, const MMA_N8_K8_F16_B &b, const MMA_M16_N8_F32_C &c)
+    template <typename I, typename J, typename K>
+    __device__ void mma_trans(MMA_M16_N8_F32_C<I, J> &d, const MMA_M16_K8_F16_A<I, K> &a, const MMA_N8_K8_F16_B<K, J> &b, const MMA_M16_N8_F32_C<I, J> &c)
     {
         mma_m16_n8_k8(d.vec4(), a.reg2(), b.reg(), c.vec4());
     }
@@ -22,7 +23,8 @@ namespace spio
     /// @param a Matrix argument A is a 16 x 16 fragment in float16.
     /// @param b Matrix argument B is an 8 x 16 fragment in float16.
     /// @param c Accumulate matrix C is a 16 x 8 fragment in float32.
-    __device__ void mma_trans(MMA_M16_N8_F32_C &d, const MMA_M16_K16_F16_A &a, const MMA_N8_K16_F16_B &b, const MMA_M16_N8_F32_C &c)
+    template <typename I, typename J, typename K>
+    __device__ void mma_trans(MMA_M16_N8_F32_C<I, J> &d, const MMA_M16_K16_F16_A<I, K> &a, const MMA_N8_K16_F16_B<K, J> &b, const MMA_M16_N8_F32_C<I, J> &c)
     {
         mma_m16_n8_k16(d.vec4(), a.reg4(), b.reg2(), c.vec4());
     }
@@ -32,7 +34,8 @@ namespace spio
     /// @param a Matrix argument A is a 16 x 16 fragment in float16.
     /// @param b Matrix argument B is an 16 x 16 fragment in float16.
     /// @param c Accumulate matrix C is a 16 x 16 fragment in float32.
-    __device__ void mma_trans(MMA_M16_N16_F32_C &d, const MMA_M16_K16_F16_A &a, const MMA_N16_K16_F16_B &b, const MMA_M16_N16_F32_C &c)
+    template <typename I, typename J, typename K>
+    __device__ void mma_trans(MMA_M16_N16_F32_C<I, J> &d, const MMA_M16_K16_F16_A<I, K> &a, const MMA_N16_K16_F16_B<K, J> &b, const MMA_M16_N16_F32_C<I, J> &c)
     {
         mma_m16_n8_k16(d.vec4(0), a.reg4(), b.reg2(0), c.vec4(0));
         mma_m16_n8_k16(d.vec4(1), a.reg4(), b.reg2(1), c.vec4(1));
